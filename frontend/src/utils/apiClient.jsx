6a2f77d4b7c83462b9d00d84b8e240547e2ddde6
Dhaneshk-ch <dhaneshkumarch1@gmail.com>
2025-12-10 18:16:12 +0530

    Update apiClient.jsx

--- frontend/src/utils/apiClient.jsx
+++ frontend/src/utils/apiClient.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'http://localhost:5001/api';
+    process.env.REACT_APP_API_BASE_URL || 'https://expro-backend.onrender.com';
 
 const apiClient = axios.create({
     baseURL: API_BASE_URL,
